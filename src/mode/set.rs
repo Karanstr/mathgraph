8a1fc21ba11c886a7ee760eab9b3eb3a2f799013
Karanstr <139508165+Karanstr@users.noreply.github.com>
2026-01-29 11:22:48 -0500

    Added number bindings for set, closes #9

--- src/mode/set.rs
+++ src/mode/set.rs
@@ -15,11 +15,28 @@ impl super::Mode for Set {
   }
 
   fn interactions(&mut self, program: &mut GraphProgram) {
+
+    if let Some(key) = get_last_key_pressed() {
+      let val = match key {
+        KeyCode::Key0 => 0,
+        KeyCode::Key1 => 1,
+        KeyCode::Key2 => 2,
+        KeyCode::Key3 => 3,
+        KeyCode::Key4 => 4,
+        KeyCode::Key5 => 5,
+        KeyCode::Key6 => 6,
+        KeyCode::Key7 => 7,
+        KeyCode::Key8 => 8,
+        KeyCode::Key9 => 9,
+        _ => self.value.val(),
+      };
+      self.value.assign(val);
+    }
     
     if let Some(node) = program.get_hovering()
       && self.value.val() <= program.max.val()
-        && is_mouse_button_pressed(MouseButton::Left)
-        && let Some(state_space) = &program.state_space
+      && is_mouse_button_pressed(MouseButton::Left)
+      && let Some(state_space) = &program.state_space
     {
       program.desired_state = state_space.set_packed(program.loaded_state, node, self.value.val());
     }
